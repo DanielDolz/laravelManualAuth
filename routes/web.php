b79b2053c69a13d77e4e901fd3337a6d23e2c98b
danieldolz <danieldolz@iesebre.com>
2016-09-27 20:59:16 +0200

    MVC: vistes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
+// CLOUSURES o FUNCIONS ANÒNIMES -> passem la funció function com a parametre
+// sense declarar-la ni posar-li nom.
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
