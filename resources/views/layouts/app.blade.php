710564bb5ea28fc1ba445c7882f4fd039d2b0494
danieldolz <danieldolz@iesebre.com>
2016-10-04 21:02:20 +0200

    layouts1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+@include('layouts.partials.htmlheader')
+
+<body>
+
+@include('layouts.partials.sidebar')
+@yield('main-content');
+
+</body>
+
+@include('layouts.partials.htmlfooter')
+
+</html>
